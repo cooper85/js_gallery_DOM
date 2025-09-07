046e6ca1f0b79dbc052c0689b3f881023066929b
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-09-07 17:27:22 +0300

    make build

---
+++ index.65d758c1.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){!function(t,e,n){document.querySelector(e).addEventListener("click",function(t){})}(0,"#thumbs .list-item .list-item__link",0)});
+//# sourceMappingURL=index.65d758c1.js.map
